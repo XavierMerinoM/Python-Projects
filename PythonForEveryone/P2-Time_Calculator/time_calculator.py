6a6b527e2e93812a372727887e1f77e7d3d0c4ea
Xavier Merino <xdmerinom@gmail.com>
2022-10-25 15:13:54 -0500

    Time Calculator V0.5-Numerical addition processed

--- PythonForEveryone/P2-Time_Calculator/time_calculator.py
+++ PythonForEveryone/P2-Time_Calculator/time_calculator.py
@@ -8,6 +8,10 @@
 # The function should add the duration time to the start
 # time and return the result.
 
+# Do not import any Python libraries. Assume that the start times
+# are valid times. The minutes in the duration time will be a whole
+# number less than 60, but the hour can be any whole number.
+
 def add_time(start, duration):
     # Initialize variables
     new_time = ''
@@ -24,14 +28,22 @@ def add_time(start, duration):
     result_minutes = add_minutes(start_time[1], duration[1])
 
     # The variables contain 'raw' results
-    # Functions return one list
+    # Functions return one list each one
     # process_minutes
     # 0: result in 60 minutes format
     # 1: hours to add
     result_minutes = process_minutes(result_minutes)
 
-    print(result_hour)
-    print(result_minutes)
+    # process_hours
+    # 0: final time result
+    # 1: how many cycles of 12 hours have the result
+    result_time = process_hours(result_hour, result_minutes[1])
+
+    # print(result_minutes)
+    # print(result_time)
+    # print(str(result_time[0]) + ':' + str(result_minutes[0]))
+
+    # new_time = format_result(result_minutes[0], result_time, start[1])
 
     return new_time
 
@@ -53,4 +65,38 @@ def add_minutes(start_minutes, duration_minutes):
     # Retuns the addition of the parameters
     return start_minutes + duration_minutes
 
-# def process_minutes(result_minutes):
+# Process result of add_minutes function
+def process_minutes(result_minutes):
+    # Initialize variables
+    # minutes has the real time in minutes
+    # hours_add has the hours to add to the main hour
+    minutes = 0
+    hours_add = 0
+
+    # Check how many hours we have to add to the final result
+    hours_add = result_minutes // 60
+    # Check the real minutes of the result
+    minutes = result_minutes % 60
+
+    return [minutes, hours_add]
+
+# Process result of add_hours function
+def process_hours(result_hour, hours_add):
+    # Initialize variables
+    # hours has the real time in hours
+    # cycles has 12-hour cycle to add to the main result
+    hours = 0
+    cycles = 0
+
+    # Add the hours got in process_minutes function
+    result_hour += hours_add
+
+    # Check how many cycles of 12 hours we have
+    cycles = result_hour // 12
+    # Check the real hours of the result
+    hours = result_hour % 12
+    # Zero does not exists in terms of hours
+    if hours == 0:
+        hours = 12
+
+    return [hours, cycles]
